de72a9267dff8e8d3fb8c2dd4541127e896c2ccb
Dinesh Panicker <dinesh.panicker@cooperlighting.com>
2024-03-25 22:39:19 +0530

    interview prep:bit manipulations

--- practize_C/self/main2.c
+++ practize_C/self/main2.c
@@ -1,6 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
+	int m = 4, n = 11, a = 0xdeadbeef, x = 0xbc; // assume n>m
+	// expected output: 0xdeadbbcf
+	printf("The previous value of a is: %x\n", a);
+	int mask1, mask2, new_mask, value_to_be_written;
+
+	mask1 = ((1 << m) - 1);		  // 0x0000000f
+	mask2 = ((1 << (n + 1)) - 1); // 0x00000fff
+	new_mask = (mask1) ^ (mask2);
+	value_to_be_written = (x << m) & new_mask; // 0x00000ab0
+
+	int new_a = (a & (~new_mask) | value_to_be_written);
+
+	printf("The new a is: %x\n", new_a);
 	return 0;
 }
